1031d68a2755911b07f0fb30f79ced51c65e1c95
Fabian Roh <fabiankmroh@gmail.com>
2019-08-29 14:46:07 +0900

    20190829 p6, p7 SOLVED

---
+++ p6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int n, k, i;
+int w[10000];
+int v[10000];
+int max = -10000;
+int status;
+
+void f(int accuW, int accuV, int i){
+    status = 0;
+
+    for(i = 0; i < n; i++){
+        if(accuW+w[i] <= k){
+            status = 1;
+            f(accuW+w[i], accuV+v[i], i);
+        }
+    }
+    
+
+    if(status == 0){
+        if(accuV > max){
+            max = accuV;
+        }
+        
+        return;
+    }
+}
+
+int main(void){
+    scanf("%d %d", &n, &k);
+
+    for(i = 0; i < n; i++){
+        scanf("%d %d", &w[i], &v[i]);
+    }
+
+    f(0,0,0);
+
+    printf("%d\n", max);
+}
